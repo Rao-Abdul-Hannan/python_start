8be57a8910c8009145579c178b0ec722830de9ab
unknown <raoabdulhannan757@gmail.com>
2024-03-09 18:55:43 +0500

    day 04

---
+++ day04.py
@@ -0,0 +1,15 @@
+name = "hannan"
+number = 12
+float = 23.2
+
+print (name," ", number, " ", float)
+
+first_name = "Abdul"
+last_name = "Hannan"
+
+print ((first_name * 3) + " " + (last_name * 3))
+
+#input function to take input, always takes input as a string
+name = input ("Enter your name: ")
+age = input ("Enter your age: ")
+print ("He is " + name + ". He is " + age + " years old.")
