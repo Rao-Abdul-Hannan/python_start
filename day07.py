d1ffc6838e5affc448755423fa04993f4eb56203
unknown <raoabdulhannan757@gmail.com>
2024-03-18 23:00:04 +0500

    day 07

---
+++ day07.py
@@ -0,0 +1,23 @@
+language = "python"
+
+# string indexing
+
+print (language[2])
+print (language[-1])  #starts from end 5 - 6 = -1
+
+# string slicing 
+# syntax -> language[start argument - end argument]
+
+print (language[2 : 4])
+print (language[-2 : 6]) # ulta nhi ja skty
+print (language[:])
+print (language[1 : ])
+print (language[ : -1])
+
+# syntax -> language[start argument - end argument]
+
+print ("Abdul-Hannan"[1 : 3])
+print ("Abdul-Hannan"[0 : 5 : 3])
+print ("Abdul-Hannan"[5 :  : -1]) # reverse string will be printed
+print ("Abdul-Hannan"[-1 :  : -1])
+print ("Abdul-Hannan"[5 :  : 1])
---
+++ practice03.py
@@ -0,0 +1,2 @@
+user_name = input ("Enter your username: ")
+print (user_name[ :  : -1]) # Reversed string
